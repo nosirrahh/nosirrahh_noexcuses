ef1815a25d44ac0997e12404e253f7d0a4b46ebf
Harrison Dias <nosirrahh@gmail.com>
2018-05-11 10:04:41 -0300

    Funcionamento básico das torres

---
+++ nosirrahh_noexcuses/Assets/Scripts/Projectile.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    #region Enumerations
+
+    public enum ProjectileState
+    {
+        None,
+        SeekingTarget,
+        Destroying
+    }
+
+    #endregion
+
+    #region Fields
+
+    public float speed = 10F;
+    
+    private Enemy target;
+    private int damage;
+    private ProjectileState state;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Awake ()
+    {
+        Rigidbody r = GetComponent<Rigidbody> ();
+        r.useGravity = false;
+        BoxCollider b = GetComponent<BoxCollider> ();
+        b.isTrigger = true;
+    }
+
+    private void Update ()
+    {
+        SeekTarget ();
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.transform == target.transform)
+        {
+            ChangeState (ProjectileState.Destroying);
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void BuildProjectile (Enemy target, int damage)
+    {
+        this.target = target;
+        this.damage = damage;
+        ChangeState (ProjectileState.SeekingTarget);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void ChangeState (ProjectileState toState)
+    {
+        if (toState == state)
+            return;
+
+        state = toState;
+
+        switch (state)
+        {
+            case ProjectileState.None:
+                break;
+            case ProjectileState.SeekingTarget:
+                break;
+            case ProjectileState.Destroying:
+                Destroy (gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SeekTarget ()
+    {
+        if (state != ProjectileState.SeekingTarget)
+            return;
+
+        if (target == null)
+        {
+            ChangeState (ProjectileState.Destroying);
+            return;
+        }
+
+        Vector3 targetDirection = (target.transform.position - transform.position).normalized;
+        Vector3 translation = targetDirection * speed * Time.deltaTime;
+        transform.Translate (translation);
+    }
+
+    #endregion
+}
---
+++ nosirrahh_noexcuses/Assets/Scripts/Tower.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    #region Enumerations
+
+    public enum TowerState
+    {
+        Building,
+        SearchingTarget,
+        Attacking
+    }
+
+    #endregion
+
+    #region Fields
+
+    public int damage;
+    public float firerate;
+    public float range;
+    public Projectile projectile;
+
+    public TowerState state;
+    public Coroutine searchTargetCoroutine;
+    public Coroutine attackCoroutine;
+
+    public Enemy target;
+    public LayerMask enemyLayerMask;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start ()
+    {
+        ChangeState (TowerState.SearchingTarget);
+    }
+
+    private void OnDrawGizmos ()
+    {
+        Gizmos.DrawWireSphere (transform.position, range);
+        Gizmos.color = Color.red;
+        if (target != null)
+            Gizmos.DrawLine (transform.position, target.transform.position);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void ChangeState (TowerState toState)
+    {
+        if (toState == state)
+            return;
+
+        state = toState;
+
+        switch (state)
+        {
+            case TowerState.Building:
+                break;
+            case TowerState.SearchingTarget:
+                searchTargetCoroutine = StartCoroutine (SearchTargetCoroutine ());
+                break;
+            case TowerState.Attacking:
+                attackCoroutine = StartCoroutine (AttackCoroutine ());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private bool IsTargetInRange ()
+    {
+        float distance = Vector3.Distance (transform.position, target.transform.position);
+        return distance <= range;
+    }
+
+    #endregion
+
+    #region Coroutine Methods
+
+    IEnumerator SearchTargetCoroutine ()
+    {
+        while (state == TowerState.SearchingTarget)
+        {
+            Debug.Log ("[Tower] SearchingTarget: " + name);
+            if (target != null)
+            {
+                ChangeState (TowerState.Attacking);
+            }
+            else
+            {
+                Collider[] colliders = Physics.OverlapSphere (transform.position, range, enemyLayerMask);
+                if (colliders != null && colliders.Length > 0)
+                {
+                    for (int i = 0; i < colliders.Length && target == null; i++)
+                        target = colliders[i].GetComponent<Enemy> ();
+                }
+                else
+                {
+                    target = null;
+                }
+            }
+            // Executa 1 vez por Update
+            yield return new WaitForSeconds (0F);
+        }
+    }
+
+    IEnumerator AttackCoroutine ()
+    {
+        while (state == TowerState.Attacking)
+        {
+            Debug.Log ("[Tower] Attacking: " + name);
+            if (target == null)
+            {
+                ChangeState (TowerState.SearchingTarget);
+            }
+            else if (!IsTargetInRange())
+            {
+                target = null;
+                ChangeState (TowerState.SearchingTarget);
+            }
+            else
+            {
+                Projectile p = Instantiate (projectile, transform.position, Quaternion.identity);
+                p.BuildProjectile (target, damage);
+                yield return new WaitForSecondsRealtime (firerate);
+            }
+        }
+    }
+
+    #endregion
+}
