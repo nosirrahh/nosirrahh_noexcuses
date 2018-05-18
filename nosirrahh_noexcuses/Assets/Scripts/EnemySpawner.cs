296e66ee0a516752fae7c6ed4ad0457b2e96169d
Harrison Dias <nosirrahh@gmail.com>
2018-05-18 18:25:00 -0300

    Adicionado classe base de EnemySpawner

---
+++ nosirrahh_noexcuses/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public WaveObject waveObject;
+        public float intervalBetweenObjects;
+    }
+
+    [System.Serializable]
+    public class WaveObject
+    {
+        public GameObject objectToSpawn;
+        public int amount;
+    }
+
+    public float initialDelay;
+    public float intervalBetweenWaves;
+    public Wave[] waves;
+}
